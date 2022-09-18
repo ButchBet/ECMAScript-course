b6952d8e30414e2006d68913750d865c64e08199
ButchBet <butchbet01@gmail.com>
2022-09-18 18:50:55 -0500

    Learned about Object.prototype.entries() and Object.prototype.values().

---
+++ src/ES8/00-objectEntries.js
@@ -0,0 +1,9 @@
+const counties = {
+    MX: 'México',
+    CO: 'Colombia',
+    CL: 'Chile',
+    PE: 'Peru',
+};
+
+console.log(Object.entries(counties));
+// [['MX', 'México'], ['CO', 'Colombia], ['CL', 'Chile'], ['PE', 'Peru']]
---
+++ src/ES8/01-objectValues.js
@@ -0,0 +1,9 @@
+const countries = {
+    MX: 'México',
+    CO: 'Colombia',
+    CL: 'Chile',
+    PE: 'Peru',
+};
+
+console.log(Object.values(countries));
+// ['México', 'Colombia', 'Chile', 'Peru']
