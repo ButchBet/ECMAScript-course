b3721ef02cc722a05057fcff452096570974b554
Butch Bet <butchbet01@gmail.com>
2022-09-26 17:28:30 -0500

    Learned about numeric separators and String.prototype.replaceAll().

---
+++ src/ES12/00-numericSeparators.js
@@ -0,0 +1,3 @@
+// Tener una lectura más amigable de cífras numéricas
+const value  = 1000_000_000;
+console.log(value);
---
+++ src/ES12/01-replaceAll.js
@@ -0,0 +1,5 @@
+const string = 'JavaScript is a wonderfull development language.';
+
+const replaceString = string.replaceAll('JavaScript', 'C++');
+console.log(string);
+console.log(replaceString);
